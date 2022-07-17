e4f649a01b9ba1c566f5619d28e5f8ce6491dd31
abdulkadir-polmayan <abdulkadirsenel63@gmail.com>
2022-07-17 18:22:09 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './App.css'
+import Header from './Components/Header';
+import Me from './Components/Me';
+import Square from './Components/Square';
+
+function App() {
+  return (
+    <div className="App">
+      <Header /> 
+      <Me /> 
+      <Square />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Me.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Me(){
+    return(
+        <div>
+            <h1>I'm 
+                <br></br>
+                <span className='me'> Abdulkadir </span>
+            </h1>
+            <h3 className='job'>
+                I'm designer and developer for  
+                <br></br>
+                    <span className='aestethic'> aestethic </span>
+                things
+            </h3>
+        </div>
+    )
+}
